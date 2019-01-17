e8494e45f6498513b81591cc0c81c5c8542c5e36
Claudia Valdivieso <claumavaldivieso@gmail.com>
2019-01-16 19:24:55 -0500

    Add "¿Qué es GraphQL"

---
+++ theme.js
@@ -0,0 +1,17 @@
+import { hack } from 'mdx-deck/themes'
+
+export default {
+  ...hack,
+
+  colors: {
+    text: '#E10098',
+    background: '#202020',
+  }
+
+  // Customize your presentation theme here.
+  //
+  // Read the docs for more info:
+  // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
+  // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
+
+}
